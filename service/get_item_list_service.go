bc85c3d68f64a54334399c5cfa73888ffde6cd56
Lanly109 <1094916227@qq.com>
2022-07-24 13:43:33 +0800

    fix: remove time sleep

--- service/get_item_list_service.go
+++ service/get_item_list_service.go
@@ -3,7 +3,6 @@ package service
 import (
 	"singo/model"
 	"singo/serializer"
-	"time"
 )
 
 // GetItemDeatilService 获得商品详细列表服务
@@ -13,7 +12,6 @@ type GetItemListService struct {
 // GetData 获取数据
 func (service *GetItemListService) GetData() serializer.Response {
 	var items []model.Item
-    time.Sleep(time.Duration(10) * time.Second)
 
 	model.DB.Find(&items)
 
